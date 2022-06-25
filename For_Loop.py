e04f11c50bccdc441857dc122c0026ae5b5cf591
Rajat Sharan <errajatsharan@gmail.com>
2022-06-25 23:38:54 +0530

    For loop

---
+++ For_Loop.py
@@ -0,0 +1,22 @@
+# For loop
+
+obj = [2, 3, 4, 5]
+for i in obj:
+    print(i*2)
+
+
+# SUM OF FIRST FIVE NATURAL NUMBER 1+2+3+4+5=15
+summation = 0
+for j in range(1, 6):
+    summation = summation+j
+print(summation)
+
+
+print("**********************************")
+
+for k in range(1, 10, 5):
+    print(k)
+    print("*****************SKIPPING FIRST INDEX**************************")
+for m in range(10):
+    print(m)
+
