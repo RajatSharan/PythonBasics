13644740329273ffe5e76191f5a197fefd582583
Rajat Sharan <errajatsharan@gmail.com>
2022-06-25 20:40:09 +0530

    If else condition

---
+++ IF_else_condition.py
@@ -0,0 +1,12 @@
+# IF condition
+
+greeting = "Good Morning"
+a= 4
+
+if a < 2:
+    print("Condition Matches")
+    print("Next Line")
+
+else:
+    print("Condition Incorrect")
+print("Independent of if else condition")
