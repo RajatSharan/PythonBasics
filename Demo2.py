75e73e5cfadb20548c2c4727861518a5b9835427
Rajat Sharan <errajatsharan@gmail.com>
2022-06-25 20:24:11 +0530

    DataTypes

--- Demo2.py
+++ Demo2.py
@@ -18,3 +18,30 @@ print(values)
 
 del values[0]
 print(values)
+
+a = (1, 2, 3, 4)
+print(a)
+print(a[0])
+
+#tuple having multiple type of data.
+b=("hello", 1,2,3,"go")
+print(b) #prints the whole tuple
+
+
+a = {1:"first name",2:"last name", "age":33}
+
+#print value having key=1
+print(a[1])
+#print value having key=2
+print(a[2])
+#print value having key="age"
+print(a["age"])
+
+#How to Create Dictionaries at run time and add data into it
+
+dic = {}
+
+dic["FirstName"] = "Rajat"
+dic["LastName"] = "Sharan"
+
+print(dic)
