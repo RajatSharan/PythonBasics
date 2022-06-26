1f3bb25ad1a04b7684a986670774600d377fa270
Rajat Sharan <errajatsharan@gmail.com>
2022-06-26 12:37:35 +0530

    Function

---
+++ Functions.py
@@ -0,0 +1,16 @@
+# In python , function is group of related statments that perform a specific task
+
+
+#Function Declaration
+
+
+def GreetMe(name):
+    print("Good Morning" + name)
+    #function call
+
+def AddIntergers(a,b):
+    return a+b
+
+GreetMe("RAJAT SHARAN")
+
+print(AddIntergers(4,5))
